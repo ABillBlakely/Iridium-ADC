4227c8606b9b130b080f3d0f0436b3588ddb3ad2
Bill Blakely <ABillBlakely@gmail.com>
2018-04-25 14:58:54 -0700

    Switch strategies from disentagle to efficient packing. 745 kSPS

--- src/pins.cpp
+++ src/pins.cpp
@@ -46,24 +46,25 @@ uint16_t DataBusClass::read()
 
 
     // port,   bit select, shift by (pin num - bit pos)
-    received |= (port_c & (1 <<  8)) >> (8);       // pc08 to bit 0
-    received |= (port_c & (1 <<  6)) >> (5);       // pc06 to bit 1
-    received |= (port_c & (1 <<  5)) >> (3);       // pc05 to bit 2
-
-    received |= (port_a & (1 << 12)) >> (9);      // pa12 to bit 3
-    received |= (port_a & (1 << 11)) >> (7);      // pa11 to bit 4
-    received |= (port_a & (1 <<  9)) >> (4);       // pa09 to bit 5
-    received |= (port_a & (1 << 10)) >> (3);      // pa10 to bit 7
-    received |= (port_a & (1 <<  8)) >> (2);       // pa08 to bit 6
-
-    received |= (port_c & ((1 <<  1)|(1 << 7))) << (7);    // pc01 to bit 8, pc07 to bit 14
-
-    received |= (port_a & (1 <<  4)) << (5);    // pa04 to bit 9
-    received |= (port_a & (1 <<  1)) << (9);   // pa01 to bit 10
-    received |= (port_a & (1 <<  0)) << (11);   // pa00 to bit 11
-    received |= (port_a & ((1 <<  6)|(1 << 7))) << (6);   // pa06 to bit 12 // pa07 to bit 13
-    received |= (port_a & (1 << 15));  // pa15 to bit 15
-
+    // efficiently pack the bits into a 16 bit container.
+    // bit order is even more messed up than before of course,
+    // but that detanglement can be handled on the pc side.
+    received |= (port_a & (1 <<  0)) ;      // pa00     to bit 0
+    received |= (port_a & (1 <<  1)) ;      // pa01     to bit 1
+    received |= (port_c & (1 <<  1)) << 2;      // pc01     to bit  2
+    received |= (port_c & (1 <<  7)) >> 4;      // pc07     to bit 3
+    received |= (port_a & (1 <<  4)) ;      // pa04     to bit 4
+    received |= (port_c & (1 <<  5)) ;      // pc05     to bit 5
+    received |= (port_a & (1 <<  6)) ;      // pa06     to bit 6
+    received |= (port_a & (1 <<  7)) ;      // pa07     to bit 7
+    received |= (port_a & (1 <<  8)) ;      // pa08     to bit 8
+    received |= (port_a & (1 <<  9)) ;      // pa09     to bit 9
+    received |= (port_a & (1 << 10)) ;      // pa10     to bit 10
+    received |= (port_a & (1 << 11)) ;      // pa11     to bit 11
+    received |= (port_a & (1 << 12)) ;      // pa12     to bit 12
+    received |= (port_c & (1 <<  6)) >> 7;      // pc06     to bit 13
+    received |= (port_c & (1 <<  8)) >> 6;      // pc08     to bit 14
+    received |= (port_a & (1 << 15)) ;      // pa15     to bit 15
     return received;
 }
 
